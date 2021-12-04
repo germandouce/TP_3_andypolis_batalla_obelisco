12a6aa8b73d61690ea30389ca8d9de5f116e8c14
German Douce <germandouce@gmail.com>
2021-12-04 19:43:18 -0300

    se agregan metodos a la clase juego

--- juego/juego.cpp
+++ juego/juego.cpp
@@ -1,1 +1,25 @@
+#include "juego.h"
 
+Juego::Juego(){
+    this-> jug_1 = new Jugador();
+    this-> jug_2 = new Jugador();
+    this -> mapa = 0;
+    this -> diccionario_edificios = 0;
+}
+
+
+int Juego:: generar_numero_random(int min, int max){
+    int range = max + 1  - min;  
+    return min + ( rand() % range);
+}
+
+void Juego::asignar_objetivos(){
+    srand( (unsigned)time(0) );
+    
+    int numero_objetivo;
+    
+    for(int i = 0; i < ojetivos->obtener_cant_objetivos(); i++){
+        numero_objetivo = generar_numero_random();
+    }
+
+}
--- juego/juego.h
+++ juego/juego.h
@@ -2,15 +2,36 @@
 #define JUEGO_H
 
 
-#include "CaminoMinimo.h"
+#include <time>
+#include ".\mapa.h"
+#include "Jugador.h"
+#include ".\diccionario.h"
 
 class Juego{
 
 private:
-  
+    Jugador * jug_1;
+    Jugador * jug_2;
+    Mapa * mapa;
+    Diccionario * diccionario_edificios;
+    //List lista_objetivos
 
 public:
+    
+    //Constructor de juego
+    Juego();
 
+    //Generar numero random
+    //PRE: Recibe los enteros "min" y "max" con los valores limite (inclusives) entre los cuales se desea generar el numero random 
+    //POST: Devuelve un valor random entre "min" y "max"
+    int generar_numero_random(int min, int max);
+
+    //Asignar objetivos
+    //PRE:
+    //POST: Asigna objetivos al jugador
+    void asignar_objetivos();
+
+};
 
 #endif //JUEGO_H
 
