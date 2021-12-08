daacd78560fb72def4d8f885653db2fb2d6d6c10
German Douce <germandouce@gmail.com>
2021-12-07 22:39:48 -0300

    se sacan archivos de mas

--- diccionario.h
+++ diccionario.h
@@ -3,13 +3,13 @@
 
 #include "nodo_dict.h"
 
-#include "edificios/aserradero.h"
-#include "edificios/escuela.h"
-#include "edificios/fabrica.h"
-#include "edificios/mina.h"
-#include "edificios/mina_oro.h"
-#include "edificios/obelisco.h"
-#include "edificios/planta_electrica.h"
+#include "registro_edificios/aserradero.h"
+#include "registro_edificios/escuela.h"
+#include "registro_edificios/fabrica.h"
+#include "registro_edificios/mina.h"
+#include "registro_edificios/mina_oro.h"
+#include "registro_edificios/obelisco.h"
+#include "registro_edificios/planta_electrica.h"
 
 const int ASCII_A = 65;
 const int ASCII_E = 69;
